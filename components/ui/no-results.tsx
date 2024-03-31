3e247d525562468eb7b5a992aec42fe06de87ee6
elkitty <hablaconwafle@gmail.com>
2024-03-31 14:00:02 -0300

    fix not found page problem

--- components/ui/no-results.tsx
+++ components/ui/no-results.tsx
@@ -1,9 +1,9 @@
 const NoResults = () => {
     return ( 
-        <div className="flex items-center justify-center h-full w-full text-neutral-500">
-            No results found. 🤷‍♂️ 🤷‍♀️ 🤷‍♂️ 🤷‍♀️ 
-        </div>
-    );
-}
- 
-export default NoResults;
+      <div className="flex items-center justify-center h-full w-full text-neutral-500">
+        No results found.
+      </div>
+     );
+  };
+   
+  export default NoResults;
